141801380c399d545d73bbb09d59e6e788d7d997
Brady Holt <brady.holt@gmail.com>
2023-02-20 08:34:48 -0700

    Improve module support in tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,16 +3,18 @@ import type { Config } from "@jest/types";
 // Sync object
 const config: Config.InitialOptions = {
   verbose: true,
-  preset: "ts-jest",
+  resolver: "ts-jest-resolver",
   testEnvironment: "node",
-  extensionsToTreatAsEsm: [".ts"],
+  extensionsToTreatAsEsm: [".ts"],  
   transform: {
     "^.+\\.(ts|tsx)?$": [
       "ts-jest",
       {
+        useESM: true,
         diagnostics: {
           ignoreCodes: ["TS151001"],
         },
+
         tsconfig: "./test/tsconfig.json",
       },
     ],
--- test/FeedHelper.test.ts
+++ test/FeedHelper.test.ts
@@ -1,22 +1,16 @@
+import { Feed } from "../src/entity/Feed";
 import { scrubFeedContent } from "../src/lib/FeedHelper";
-describe("FeedHelper", () => {
-  it("foo", () => {
-    const feed = {
-      title: "Test",
-      description: "Test",
-      link: "https://example.com",
-      items: [],
-    };
-    const item = {
-      id: "Test",
-      title: "Test",
-      description: "Test",
-      link: "https://example.com",
-      pubDate: new Date(),
-      content: 'This is a test. <img src="/foo.jpg" /> This is only a test.',
-    };
 
-    const expected = 'This is a test. <img src="https://example.com/foo.jpg" /> This is only a test.';
-    expect(scrubFeedContent(feed, item)).toBe(expected);
+describe("FeedHelper", () => {
+  it("scrubFeedContent", () => {    
+    const feed = new Feed();
+    feed.id = 1;
+    feed.title = "Test";
+    feed.url = "https://example.com";
+    feed.createdAtEpoch = 123;
+
+    expect(scrubFeedContent(feed, `This is a test. <img src="/foo.jpg" /> This is only a test.`)).toBe(
+      `This is a test. <img src="https://example.com/foo.jpg" /> This is only a test.`
+    );
   });
 });
